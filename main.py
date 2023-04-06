c8bebb3759f4642065915966bb8998dcf6323fc4
Devesh Gautam <deveshgautamking@gmail.com>
2023-04-06 16:32:46 +0530

    Add Images,ErrorHanding,show Temp and Sky

--- main.py
+++ main.py
@@ -12,9 +12,26 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select the data to view", 
                       ("Temperature", "Sky"))
 
-st.subheader(f"{option} for the next {days} days in {place}")
+if place:
+    try:
+        filtered_data = get_data(place, days)
 
-data = get_data(place, days, option)
+        st.subheader(f"{option} for the next {days} days in {place}")
 
-figure = px.line(x=d, y=t, labels={"x":"Dates", "y": "Temperatur (C)"} )
-st.plotly_chart(figure)
+        if option == "Temperature":
+            temperature = [dict["main"]["temp"]/10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperature, labels={"x":"Dates", "y": "Temperatur (C)"} )
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            images = {"Clear":"images/clear.png",
+                        "Clouds":"images/cloud.png",
+                        "Rain":"images/rain.png", 
+                        "Snow":"images/snow.png"}
+                
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            images_path = [images[condition] for condition in sky_conditions]
+            st.image(images_path, width=115)
+    except KeyError:
+        st.error('That Place does not exist.', icon="🚨")
